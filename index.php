9ae646aed52b575855916cf9a9f220eb9e7b6677
andrei.vupt <andrei.vupt@gmail.com>
2018-07-08 21:51:06 -0300

    CRUD criado

--- index.php
+++ index.php
@@ -60,9 +60,13 @@ $app->get("/admin/users", function () {
 
     User::verifyLogin();
 
+    $users = User::listAll();
+
     $page = new PageAdmin();
 
-    $page->setTpl("users");
+    $page->setTpl("users", array(
+        "users"=>$users
+    ));
 });
 
 $app->get("/admin/create", function () {
@@ -75,29 +79,68 @@ $app->get("/admin/create", function () {
 
 });
 
+$app->get("/admin/users/:iduser/delete", function ($iduser){
+
+    User::verifyLogin();
+
+    $user = new User();
+
+    $user->get((int)$iduser);
+
+    $user->delete();
+
+    header("Location: /admin/users");
+    exit();
+});
+
+
 $app->get("/admin/users/:iduser", function ($iduser) {
 
     User::verifyLogin();
 
+    $user = new User();
+
+    $user->get((int)$iduser);
+
     $page = new PageAdmin();
 
-    $page->setTpl("users-update");
+    $page->setTpl("users-update", array(
+        "user"=>$user->getValues()
+    ));
 
 });
 
 $app->post("/admin/users/create", function (){
 
     User::verifyLogin();
+
+    $user = new User();
+
+    $_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+    $user->setData($_POST);
+
+    $user->save();
+
+    header("Location: /admin/users");
+    exit();
 });
 
 $app->post("/admin/users/:iduser", function ($iduser){
 
     User::verifyLogin();
-});
 
-$app->delete("/admin/users/:iduser", function ($iduser){
+    $user = new User();
 
-    User::verifyLogin();
+    $_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+    $user->get((int)$iduser);
+
+    $user->setData($_POST);
+
+    $user->update();
+    header("Location: /admin/users");
+    exit();
 });
 
 $app->run();
